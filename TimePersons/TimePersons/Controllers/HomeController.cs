4bcb1340fb1fd3a45d8703bf66980a6ffd90dafb
cdcummings10 <ccummings0913@gmail.com>
2019-10-29 11:07:03 -0700

    Finished basic functionality of site.

--- TimePersons/TimePersons/Controllers/HomeController.cs
+++ TimePersons/TimePersons/Controllers/HomeController.cs
@@ -3,14 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TimePersons.Models;
 
 namespace TimePersons.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int startYear, int endYear)
+        {
+            ChosenYears chosenYears = new ChosenYears(startYear, endYear);
+            return RedirectToAction("Results", chosenYears);
+        }
+
+        public ViewResult Results(ChosenYears years)
+        {
+            TimePerson timePerson = new TimePerson();
+            timePerson.GetPersons(years);
+            return View(timePerson.GetPersons(years));
         }
     }
 }
